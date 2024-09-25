ba15eb62e69cbfdc87149c54b92979c7c0efbe1c
tieminnom8520 <nghiempham0404@gmail.com>
2024-09-25 19:33:55 +0700

    Initial commit

---
+++ src/test/java/com/example/J2EE_project/J2EeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.J2EE_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2EeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
