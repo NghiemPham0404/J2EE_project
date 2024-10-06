c9f3cf8de954cc70b36c356de83db8661d2a52eb
tieminnom8520 <nghiempham0404@gmail.com>
2024-10-06 19:47:24 +0700

    update 6/10/2024
    update Account Service

--- src/main/java/com/example/J2EE_project/services/AccountService.java
+++ src/main/java/com/example/J2EE_project/services/AccountService.java
@@ -18,53 +18,95 @@ public class AccountService implements serviceInterface<Account, Integer>{
 
     @Autowired
     AccountRepository accountRepository;
-
+    /**
+     * Thêm mới một tài khoản
+     */
     @Override
     public String create(Account account) {
         accountRepository.save(account);
         return "account created successfully";
     }
 
+    /**
+     *Cập nhật thông tin tài khoản
+     */
     @Override
     public String update(Account account) {
         accountRepository.save(account);
         return "account updated successfully";
     }
 
+    /**
+     *Xóa thông tin tài khoản
+     */
     @Override
     public String delete(Integer id) {
         accountRepository.deleteById(id);
         return "account deleted successfully";
     }
 
-    @Override
-    public List<Account> listAll() {
-        return accountRepository.findAll();
-    }
-
-    @Override
-    public List<Account> listAllNewest() {
-        List<Account> accountList = accountRepository.findAll();
-        accountList.sort(Comparator.comparingInt(Account::getId));
-        return accountList;
-    }
-
-    @Override
-    public Page<Account> listAll(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
-        return accountRepository.findAll(pageable);
-    }
-
-    @Override
-    public Page<Account> listAllNewest(int page) {
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "id"));
-        return accountRepository.findAll(pageable);
-    }
-
+    /**
+     *Lấy tài khoản theo id
+     */
     @Override
     public Account get(Integer id) {
         Account account = accountRepository.findById(id).get();
         System.out.println("Ten la : "+ account.getName());
         return account;
+    }
+
+    public Page<Account> listAllAccount(int id, int page){
+        Pageable pageable = PageRequest.of(page, 10);
+        return accountRepository.listAllAccount(id, pageable);
+    }
+
+    /**
+     *Lấy tất cả tài khoản theo tên
+     */
+    public Page<Account> listByName(String name, int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return accountRepository.findByNameIgnoreCase(name, pageable);
+    }
+
+     /**
+     *Lấy tất cả tài khoản theo role
+     */
+    public Page<Account> listByRole(int role_id, int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return accountRepository.findByRole(role_id, pageable);
+    }
+
+    /**
+     *Khóa tài khoản
+     */
+    public void deactivateAccount(int id) {
+        accountRepository.updateActiveStatus(id, false);
+    }
+
+    /**
+     *Mở khóa tài khoản
+     */
+    public void activateAccount(int id) {
+        accountRepository.updateActiveStatus(id, true);
+    }
+
+    @Override
+    public List<Account> listAll() {
+        return null;
+    }
+
+    @Override
+    public List<Account> listAllNewest() {
+        return null;
+    }
+
+    @Override
+    public Page<Account> listAll(int page) {
+       return null;
+    }
+
+    @Override
+    public Page<Account> listAllNewest(int page) {
+        return null;
     }
 }
